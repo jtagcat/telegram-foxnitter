816b9ace71fd2e4b47b23991bdc27877770c6dad
jtagcat <git-12dbd862@jtag.cat>
2022-02-06 13:32:13 +0200

    twitter proto oauth1?

--- go.mod
+++ go.mod
@@ -5,16 +5,9 @@ go 1.17
 require github.com/dghubble/go-twitter v0.0.0-20211115160449-93a8679adecb
 
 require (
-	github.com/golang/protobuf v1.4.2 // indirect
-	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
-	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
-)
-
-require (
 	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
+	github.com/dghubble/oauth1 v0.7.1
 	github.com/dghubble/sling v1.4.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/rs/zerolog v1.26.1
-	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
 )
--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"os"
 
-	"github.com/dghubble/go-twitter/twitter"
 	"github.com/rs/zerolog/log"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
+
+	// "golang.org/x/oauth2"
+	// "golang.org/x/oauth2/clientcredentials"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
 
 func main() {
 	// oauth2 configures a client that uses app credentials to keep a fresh token
-	config := &clientcredentials.Config{
-		ClientID:     os.Getenv("TWITTER_CONSUMER_KEY"),
-		ClientSecret: os.Getenv("TWITTER_CONSUMER_SECRET"),
-		TokenURL:     "https://api.twitter.com/oauth2/token",
-	}
+	// config := &clientcredentials.Config{
+	// 	ClientID:     os.Getenv("TWITTER_CONSUMER_KEY"),
+	// 	ClientSecret: os.Getenv("TWITTER_CONSUMER_SECRET"),
+	// 	TokenURL:     "https://api.twitter.com/oauth2/token",
+	// }
 	// http.Client will automatically authorize Requests
-	httpClient := config.Client(oauth2.NoContext)
+	// httpClient := config.Client(oauth2.NoContext)
+
+	config := oauth1.NewConfig(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"))
+	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
+	// http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth1.NoContext, token)
+
 	// Twitter client
 	c := twitter.NewClient(httpClient)
 
